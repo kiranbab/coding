7586607c06c36c7da923cc9518227b298e5ed227
kiranbabu1 <kiran2babu@gmail.com>
2023-03-09 15:38:15 +0530

    folder restructure

--- snake/main.py
+++ snake/main.py
@@ -8,7 +8,7 @@ SIZE = 40
 
 class Apple:
     def __init__(self, surface):
-        self.apple = pygame.image.load('./python_projects/resources/apple.jpg').convert()
+        self.apple = pygame.image.load('./resources/apple.jpg').convert()
         self.parent_screen = surface
         self.x = SIZE * 3
         self.y = SIZE * 3
@@ -26,7 +26,8 @@ class Snake:
     def __init__(self, surface, length):
         self.length = length
         self.parent_screen = surface
-        self.block = pygame.image.load('./python_projects/resources/block.jpg').convert()
+        self.block = pygame.image.load(
+            './resources/block.jpg').convert()
         self.x = [SIZE] * length
         self.y = [SIZE] * length
         self.direction = 'down'
@@ -89,7 +90,7 @@ class Game:
         return False
 
     def play_backgorund_music(self):
-        pygame.mixer.music.load('./python_projects/resources/bg_music_1.mp3')
+        pygame.mixer.music.load('./resources/bg_music_1.mp3')
         pygame.mixer.music.play()
 
     def play(self):
@@ -99,7 +100,7 @@ class Game:
         pygame.display.flip()
 
         if self.is_collision(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
-            sound=pygame.mixer.Sound('./python_projects/resources/ding.mp3')
+            sound = pygame.mixer.Sound('./resources/ding.mp3')
             pygame.mixer.Sound.play(sound)
             # print("Collision occured")
             self.snake.increase_length()
@@ -109,33 +110,37 @@ class Game:
             if self.is_collision(self.snake.x[0], self.snake.y[0], self.snake.x[i], self.snake.y[i]):
                 # print("game over, you hit youself")
                 # exit(0)s
-                crash_sound=pygame.mixer.Sound("./python_projects/resources/crash.mp3")
+                crash_sound = pygame.mixer.Sound(
+                    "./resources/crash.mp3")
                 pygame.mixer.Sound.play(crash_sound)
                 raise "Game Over"
 
     def display_score(self):
         font = pygame.font.SysFont('arial', 30)
-        score = font.render(f"Score: {self.snake.length}", True, (200, 200, 200))
+        score = font.render(
+            f"Score: {self.snake.length}", True, (200, 200, 200))
         self.surface.blit(score, (800, 10))
 
     def show_game_over(self):
         self.surface.fill((8, 163, 161))
-        font=pygame.font.SysFont('arial',30)
-        line1= font.render(f"Game is over! Your score is {self.snake.length}",True,(200,200,200))
-        self.surface.blit(line1,(200,300))
-        line2 = font.render("To play game again press Enter. To exit press Escape!",True,(200,200,200))
-        self.surface.blit(line2,(200,350))
+        font = pygame.font.SysFont('arial', 30)
+        line1 = font.render(
+            f"Game is over! Your score is {self.snake.length}", True, (200, 200, 200))
+        self.surface.blit(line1, (200, 300))
+        line2 = font.render(
+            "To play game again press Enter. To exit press Escape!", True, (200, 200, 200))
+        self.surface.blit(line2, (200, 350))
         pygame.display.flip()
 
         pygame.mixer.music.pause()
 
     def reset(self):
-        self.snake=Snake(self.surface,1)
-        self.apple=Apple(self.surface)
+        self.snake = Snake(self.surface, 1)
+        self.apple = Apple(self.surface)
 
     def run(self):
         running = True
-        pause=False
+        pause = False
         while running:
             for event in pygame.event.get():
                 if event.type == KEYDOWN:
@@ -152,9 +157,8 @@ class Game:
                             self.snake.move_right()
 
                     if event.key == K_RETURN:
-                        pause=False
+                        pause = False
                         pygame.mixer.music.unpause()
-
 
                 elif event.type == QUIT:
                     running = False
@@ -164,7 +168,7 @@ class Game:
                     self.play()
             except Exception as e:
                 self.show_game_over()
-                pause=True
+                pause = True
                 self.reset()
 
             time.sleep(0.3)
